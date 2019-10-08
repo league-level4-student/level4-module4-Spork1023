32491e1c1d330ccdbd7d4734a793613f194f6aaa
league <league@imac-10.attlocal.net>
2019-10-08 16:56:54 -0700

    Worked on encapsulate data module 4

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -7,7 +7,7 @@ package _01_introduction_to_encapsulation;
  * To encapsulate a member variable, follow these three steps:
  * 
  * 1. Make the member variable private (or protected)
- * 
+ *
  * 2. Make a getter and setter method (accessor and mutator) for the variable.
  * 
  * 3. Add restrictions to the setter method so the member variable cannot be 
@@ -23,11 +23,59 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
 	
+	public int getItemsReceived() {
+		return itemsReceived;
+	}
+
+	public void setItemsReceived(int itemsReceived) {
+		if(itemsReceived > 0) {
+		this.itemsReceived = itemsReceived;
+		}
+		else {
+			this.itemsReceived = 0;
+		}
+	}
+
+	public float getDegreesTurned() {
+		return degreesTurned;
+	}
+
+	public void setDegreesTurned(float degreesTurned) {
+		if(degreesTurned > 0.0 && degreesTurned < 360.0) {
+		this.degreesTurned = degreesTurned;
+		}
+	}
+
+	public String getNomenclature() {
+		return nomenclature;
+	}
+
+	public void setNomenclature(String nomenclature) {
+		if(!nomenclature.equals("")) {
+		this.nomenclature = nomenclature;
+		}
+		else {
+			this.nomenclature = " ";
+		}
+	}
+
+	public Object getMemberObj() {
+		return memberObj;
+	}
+
+	public void setMemberObj(Object memberObj) {
+		System.out.println(memberObj.toString());
+		if(memberObj.toString() != null) {
+		this.memberObj = memberObj;
+		
+		}
+	}
+
 	public static void main(String[] args) {
 		
 	}
---
+++ src/_01_introduction_to_encapsulation/runner.java
@@ -0,0 +1,9 @@
+package _01_introduction_to_encapsulation;
+
+public class runner {
+	
+	public static void main(String[] args) {
+		EncapsulateTheData ed = new EncapsulateTheData();
+		ed.setMemberObj(new String("Hello"));
+	}
+}
